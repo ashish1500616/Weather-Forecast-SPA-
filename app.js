6723e25ce92d39d67e61ab888a20bad292fa2be1
Ashish Verma <ashish1500616@gmail.com>
2017-11-04 11:13:22 +0530

    ADD controllers Custom Service and Routes

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 //Add Module,
 
-var weatherApp=angular.module('weatherApp',[])
+
+var weatherApp = angular.module('weatherApp', ['ngRoute','ngResource']);
+
+weatherApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/home.htm',
+            controller: 'mainController'
+        })
+        .when('/forecast', {
+            templateUrl: 'pages/forecast.htm',
+            controller: 'secondController'
+        })
+});
+
+weatherApp.service('cityService',function(){
+    this.city="New Delhi,ND";
+});
+weatherApp.controller('mainController', ['$scope', '$log','cityService', function($scope, $log,cityService) {
+    $scope.name = 'Home';
+    $scope.city=cityService.city;
+    $scope.$watch('city',function(){
+        cityService.city=$scope.city;
+    })
+}]);
+weatherApp.controller('secondController', ['$scope', '$log','cityService', function($scope, $log,cityService) {
+    $scope.name = 'Forecast';
+    $scope.city=cityService.city;
+}]);
+ 
